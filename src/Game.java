4dfc0da70b00e2f7b01b889201b006311647b369
joshho0701 <70684309+joshho0701@users.noreply.github.com>
2026-01-09 14:38:48 +0100

    Enhance Game initialization and combat messages

--- src/Game.java
+++ src/Game.java
@@ -7,34 +7,40 @@ import Utils.RandomUtils;
 
 public class Game {
     private final Player _player;
-
     private Scanner _input;
 
     public Game() {
         this._input = new Scanner(System.in);
 
+        System.out.println("=== Welcome to the Java RPG ===");
         System.out.println("Enter your character's name:");
         String userName = _input.nextLine();
 
-        this._player = new Player(userName, 25, 1, 10);
+        this._player = new Player(userName, 100, 10, 15); 
     }
 
     public void start() {
-        System.out.println("Game started!");
+        System.out.println("\n--- Your journey begins, " + _player.getName() + "! ---");
 
         while (_player.isAlive()) {
-            if (RandomUtils.chance(0.25)) {
-                System.out.println("An enemy appears!");
+            try { Thread.sleep(800); } catch (InterruptedException e) {}
+
+            if (RandomUtils.chance(0.30)) { 
+                System.out.println("\n[!] The air grows cold... Something is approaching!");
 
                 Enemy enemy = EnemyFactory.createRandomEncounter();
+
+                System.out.println(">> A " + enemy.getName() + " blocks your path!");
 
                 Combat combatLoop = new Combat(_player, enemy);
                 combatLoop.start();
             } else {
-                System.out.println("Exploring...");
+                System.out.println(" You travel deeper into the wilderness...");
             }
         }
 
-        System.out.println("Game over!");
+        System.out.println("\n==================================");
+        System.out.println(" GAME OVER - Your legend has ended.");
+        System.out.println("==================================");
     }
 }
